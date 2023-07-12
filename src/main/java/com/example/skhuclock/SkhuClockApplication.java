f9e47aa488ec3445742b9a2960b0d18d2d8f7353
MyoungJinSeo <sih5673@gmail.com>
2023-07-12 15:15:01 +0900

    Start Setting

---
+++ src/main/java/com/example/skhuclock/SkhuClockApplication.java
@@ -0,0 +1,13 @@
+package com.example.skhuclock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkhuClockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkhuClockApplication.class, args);
+    }
+
+}
