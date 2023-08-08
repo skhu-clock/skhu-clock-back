e859208acb45f2b3b0e55e860f2bef2bdb830f95
tndusy27 <tndusy527@gmail.com>
2023-08-08 11:53:20 +0900

    refactor : 코드 수정 #3

--- src/main/java/com/example/skhuclock/api/crawling/controller/NoticeController.java
+++ src/main/java/com/example/skhuclock/api/crawling/controller/NoticeController.java
@@ -12,12 +12,10 @@ import java.io.IOException;
 
 @RequiredArgsConstructor
 @RestController
-//@CrossOrigin(origins = "http://localhost:3000/") //웹 페이지의 제한된 자원을 외부 도메인에서 접근을 허용해주는 매커니즘
 @RequestMapping("/crawling")
 public class NoticeController {
 
     private final NoticeService noticeService;
-
 
     // 크롤링한 공지사항 정보 전체 조회
     @GetMapping("/notices")
