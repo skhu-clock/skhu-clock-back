e8de62c478e229d69969767ec4f5f7e6542d9d6a
tndusy27 <tndusy527@gmail.com>
2023-08-08 14:53:00 +0900

    refactor : @id 변경 Long -> String #9

--- src/main/java/com/example/skhuclock/domain/Crawling/NoticeRepository.java
+++ src/main/java/com/example/skhuclock/domain/Crawling/NoticeRepository.java
@@ -1,5 +1,5 @@
 package com.example.skhuclock.domain.Crawling;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-public interface NoticeRepository extends JpaRepository<Notice, Long> {
+public interface NoticeRepository extends JpaRepository<Notice, String> {
 }
