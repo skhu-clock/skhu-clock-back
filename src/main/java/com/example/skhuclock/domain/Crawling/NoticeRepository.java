8488d19656820f190ea6c2d1f041c063bc930082
tndusy27 <tndusy527@gmail.com>
2023-08-08 11:52:38 +0900

    refactor : 코드 수정 #3

--- src/main/java/com/example/skhuclock/domain/Crawling/NoticeRepository.java
+++ src/main/java/com/example/skhuclock/domain/Crawling/NoticeRepository.java
@@ -1,8 +1,5 @@
 package com.example.skhuclock.domain.Crawling;
 
-import com.example.skhuclock.domain.Crawling.Notice;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
-
 }
