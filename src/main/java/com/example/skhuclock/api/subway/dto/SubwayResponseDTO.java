937bc75819456185c3495be212e5981c916cb5a5
tndusy27 <tndusy527@gmail.com>
2023-08-11 16:16:32 +0900

    feat : 지하철 DTO #16

---
+++ src/main/java/com/example/skhuclock/api/subway/dto/SubwayResponseDTO.java
@@ -0,0 +1,13 @@
+package com.example.skhuclock.api.subway.dto;
+
+
+import com.example.skhuclock.domain.Subway.Subway;
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class SubwayResponseDTO {
+
+    private Subway subway;
+}
