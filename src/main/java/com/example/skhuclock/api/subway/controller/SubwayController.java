d67df8df8c999f7d50cab0736898a0acfe630cb3
tndusy27 <tndusy527@gmail.com>
2023-08-11 16:14:37 +0900

    feat : 지하철 API Controller #16

---
+++ src/main/java/com/example/skhuclock/api/subway/controller/SubwayController.java
@@ -0,0 +1,40 @@
+package com.example.skhuclock.api.subway.controller;
+
+
+import com.example.skhuclock.api.subway.dto.SubwayResponseDTO;
+import com.example.skhuclock.api.subway.service.SubwayService;
+import com.example.skhuclock.domain.Subway.Subway;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("api/subway")
+@Slf4j
+@Tag(name = "Subway", description = "지하철 API Document")
+public class SubwayController {
+    private final SubwayService subwayService;
+
+    @GetMapping
+    @Operation(summary = "subway json", description = "지하철 정보 출력")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "조회 성공", content = @Content(schema = @Schema(implementation = Subway.class))),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(implementation = Error.class)))
+    })
+    public ResponseEntity<?> getSubway() {
+        List<SubwayResponseDTO> dto = subwayService.getSubway();
+        return ResponseEntity.ok(dto);
+    }
+}
