e88b2b2e5d713f8fd21ad2d34a255c89847e7692
Jakyung <rlawkrud15@gmail.com>
2023-08-14 16:28:52 +0900

    Delete : subway repo

--- src/main/java/com/example/skhuclock/domain/Subway/SubwayRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.skhuclock.domain.Subway;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SubwayRepository extends JpaRepository<Subway,Long> {
-
-}
