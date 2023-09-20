964bb73c6d411e66f37455c3b0c8515971e530c1
myoungjinseo <sih5673@gmail.com>
2023-09-21 00:25:04 +0900

    fix : 시간에러 #50

--- src/main/java/com/example/skhuclock/SkhuClockApplication.java
+++ src/main/java/com/example/skhuclock/SkhuClockApplication.java
@@ -1,15 +1,23 @@
 package com.example.skhuclock;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaAuditing
+@Slf4j
 public class SkhuClockApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SkhuClockApplication.class, args);
     }
 
+    private static void init() {
+        log.info("Spring Server TimeZone : Asia/Seoul");
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 }
