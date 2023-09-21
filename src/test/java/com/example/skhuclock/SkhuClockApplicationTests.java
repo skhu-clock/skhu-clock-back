ba5b269180a2ca53d5455a66d5dcc07dde31d65b
myoungjinseo <sih5673@gmail.com>
2023-09-21 17:57:45 +0900

    delete : test #53

--- src/test/java/com/example/skhuclock/SkhuClockApplicationTests.java
+++ src/test/java/com/example/skhuclock/SkhuClockApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.skhuclock;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SkhuClockApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.example.skhuclock;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SkhuClockApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
